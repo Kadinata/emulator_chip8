40ee5a20b90b7cacc1935275faf9a80501e8668d
Kadinata <2396847+Kadinata@users.noreply.github.com>
2024-12-08 18:52:24 -0800

    Cleaning up timer module and adding documentations

--- include/timer.h
+++ include/timer.h
@@ -5,14 +5,33 @@
 #include <sys/time.h>
 #include "status_code.h"
 
+/** Timer data structure definitions */
 typedef struct timer_s
 {
+  /** Period of this timer in microseconds */
   uint64_t period_us;
-  struct timeval start;
-  struct timeval end;
+
+  /** Timestamp of the last tick of this timer */
+  struct timeval timestamp;
 } timer_t;
 
-status_code_t timer_init(timer_t *const timer, uint32_t timer_freq_hz);
+/**
+ * Initialize a timer instance and sets its frequency.
+ * @param timer - Pointer to the timer object to initialize.
+ * @param timer_freq_hz - Desired frequency for the timer.
+ * @return STATUS_OK if initialization is successful, otherwise appropriate error code.
+ */
+status_code_t timer_init(timer_t *const timer, uint32_t const timer_freq_hz);
+
+/**
+ * Check whether the amount of elapsed time has exceeded the timer's period
+ * since the last tick of the timer. This function should be called
+ * in the main loop as often as possible to ensure the timer runs as close as
+ * possible to its desired frequency.
+ * @param timer - Pointer to the timer object to check.
+ * @return 1 if the amount of time between now and the timer's last tick has
+ *         exceeded the timer's period, 0 otherwise.
+ */
 uint8_t timer_check(timer_t *const timer);
 
 #endif /* __TIMER_H__ */
--- src/timer.c
+++ src/timer.c
@@ -3,12 +3,18 @@
 #include "timer.h"
 #include "status_code.h"
 
+#define USEC_PER_SEC (1000000)
+
+/**
+ * Helper function to calculate the amount of time that has elapsed between 
+ * two timestamps and return the difference in microseconds.
+ */
 static inline uint64_t time_diff(struct timeval * const start, struct timeval * const end)
 {
-  return (uint64_t)((end->tv_sec - start->tv_sec) * 1000000) + (uint64_t)(end->tv_usec - start->tv_usec);
+  return (uint64_t)((end->tv_sec - start->tv_sec) * USEC_PER_SEC) + (uint64_t)(end->tv_usec - start->tv_usec);
 }
 
-status_code_t timer_init(timer_t *const timer, uint32_t timer_freq_hz)
+status_code_t timer_init(timer_t *const timer, uint32_t const timer_freq_hz)
 {
   VERIFY_PTR_RETURN_ERROR_IF_NULL(timer);
 
@@ -17,17 +23,18 @@ status_code_t timer_init(timer_t *const timer, uint32_t timer_freq_hz)
   }
 
   timer->period_us = 1000000 / timer_freq_hz;
-  gettimeofday(&timer->start, NULL);
+  gettimeofday(&timer->timestamp, NULL);
 
   return STATUS_OK;
 }
 
 uint8_t timer_check(timer_t *const timer) 
 {
-  gettimeofday(&timer->end, NULL);
+  struct timeval current;
+  gettimeofday(&current, NULL);
 
-  if (time_diff(&timer->start, &timer->end) >= timer->period_us) {
-    gettimeofday(&timer->start, NULL);
+  if (time_diff(&timer->timestamp, &current) >= timer->period_us) {
+    gettimeofday(&timer->timestamp, NULL);
     return 1;
   }
 
